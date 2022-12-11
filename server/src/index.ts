681c21463ab97ba4be047d39e78c3e80410ed83b
Lucas Sellari <lucassellari@Lucass-MacBook-Pro.local>
2022-12-10 21:29:52 -0300

    setup graphql: express with apollo server and type-graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,42 @@
 import { MikroORM } from "@mikro-orm/core";
+import { __port__ } from "./constants";
 import options from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
-import { Post } from "./entities/Post";
+
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import http from "http";
+import cors from "cors";
+import { json } from "body-parser";
 
 const main = async () => {
   const orm = await MikroORM.init(options);
   await orm.getMigrator().up();
 
-  // const post = orm.em.create(Post, { title: "my first post!" });
-  // await orm.em.persistAndFlush(post);
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  });
+  await apolloServer.start();
+  app.use(
+      "/graphql",
+      cors<cors.CorsRequest>(),
+      json(),
+      expressMiddleware(apolloServer, {
+        context: async ({ req }) => ({ token: req.headers.token }),
+      }),
+    );
 
-  // const myPost = await orm.em.find(Post,  {});
-  // console.log(myPost)
+  app.listen(__port__, () => {
+    console.log(`🚀 server started at http://localhost:${__port__}`);
+  });
 }
 
 main().catch(err => { console.error(err) });
